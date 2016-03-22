6770d157e85158149f1ece4d0037e0dc3d048928
duong <hoangthaiduong@gmail.com>
2016-03-22 15:11:01 -0600

    Add Vector2 type.

--- src/hana/core/vector.h
+++ src/hana/core/vector.h
@@ -1,10 +1,99 @@
 #pragma once
 
+#include "assert.h"
 #include <cstdint>
 #include <iosfwd>
 
 namespace hana { namespace core {
 
+/** 2D vector */
+template <typename T>
+struct Vector2 {
+    T x = 0;
+    T y = 0;
+
+    Vector2() = default;
+    Vector2(T x_, T y_) : x(x_), y(y_) {}
+    template <typename U>
+    Vector2(const Vector2<U>& other) : x(other.x), y(other.y) {}
+    template <typename U>
+    Vector2& operator=(const Vector2<U>& other) { x = other.x; y = other.y; }
+};
+
+template <typename T>
+bool operator<=(const Vector2<T>& a, const Vector2<T>& b)
+{
+    return a.x <= b.x && a.y <= b.y;
+}
+
+template <typename T>
+bool operator==(const Vector2<T>& a, const Vector2<T>& b)
+{
+    return a.x == b.x && a.y == b.y;
+}
+
+template <typename T>
+Vector2<T> operator+(const Vector2<T>& a, T x)
+{
+    return Vector2<T>(a.x + x, a.y + x);
+}
+
+template <typename T>
+Vector2<T> operator+(const Vector2<T>& a, const Vector2<T>& b)
+{
+    return Vector2<T>(a.x + b.x, a.y + b.y);
+}
+
+template <typename T>
+Vector2<T> operator-(const Vector2<T>& a, T x)
+{
+    return Vector3<T>(a.x - x, a.y - x);
+}
+
+template <typename T>
+Vector2<T> operator-(const Vector2<T>& a, const Vector2<T>& b)
+{
+    return Vector2<T>(a.x - b.x, a.y - b.y);
+}
+
+template <typename T>
+Vector2<T> operator*(const Vector2<T>& a, const Vector2<T>& b)
+{
+    return Vector2<T>(a.x * b.x, a.y * b.y);
+}
+
+template <typename T>
+Vector2<T> operator*(const Vector2<T>& a, T x)
+{
+    return Vector2<T>(a.x * x, a.y * x);
+}
+
+template <typename T>
+Vector2<T> operator/(const Vector2<T>& a, const Vector2<T>& b)
+{
+    T x = b.x != 0 ? a.x / b.x : 0;
+    T y = b.y != 0 ? a.y / b.y : 0;
+    return Vector3<T>(x, y);
+}
+
+using Vector2u64 = Vector2<uint64_t>;
+using Vector2i = Vector2<int>;
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const Vector2<T>& v)
+{
+    os << v.x << " " << v.y;
+    return os;
+}
+
+template <typename T>
+std::istream& operator >> (std::istream& os, Vector2<T>& v)
+{
+    os >> v.x >> v.y;
+    return os;
+}
+
+/** 3D vector */
 template <typename T>
 struct Vector3 {
     T x = 0;
