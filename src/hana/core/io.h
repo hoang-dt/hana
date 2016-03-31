e10f8624a38964785af3ed10698a1d7bfc692093
duong <hoangthaiduong@gmail.com>
2016-03-31 16:30:01 -0600

    Add more I/O functions to io.h to handle POD-like types and non POD-like types

--- src/hana/core/io.h
+++ src/hana/core/io.h
@@ -7,36 +7,49 @@
 
 namespace hana { namespace core {
 
+template<bool B, typename T = void> using disable_if = std::enable_if<!B, T>;
+
+/** Read a POD-like variable from a FILE stream */
 template <typename T>
-void read(FILE* stream, T* data, size_t count = 1)
+typename std::enable_if<std::is_trivially_copyable<T>::value>::type
+read(FILE* stream, T* data, size_t count = 1)
 {
     fread(data, sizeof(T), count, stream);
 }
 
+/** Write a POD-like variable to a FILE stream */
 template <typename T>
-void write(FILE* stream, const T* data, size_t count = 1)
+typename std::enable_if<std::is_trivially_copyable<T>::value>::type
+write(FILE* stream, const T* data, size_t count = 1)
 {
     fwrite(data, sizeof(T), count, stream);
 }
 
+/** Read a POD-like variable from a stream */
 template <typename T>
-void read(std::istream& is, T* data, size_t count = 1)
+typename std::enable_if<std::is_trivially_copyable<T>::value>::type
+read(std::istream& is, T* data, size_t count = 1)
 {
     is.read(reinterpret_cast<char*>(data), sizeof(T) * count);
 }
 
+/** Write a POD-like variable to a stream */
 template <typename T>
-void write(std::ostream& os, const T* data, size_t count = 1)
+typename std::enable_if<std::is_trivially_copyable<T>::value>::type
+write(std::ostream& os, const T* data, size_t count = 1)
 {
     os.write(reinterpret_cast<const char*>(data), sizeof(T) * count);
 }
 
+/** Write a POD-like variable to a stream */
 template <typename T>
-void write(std::ostream& os, T data)
+typename std::enable_if<std::is_trivially_copyable<T>::value>::type
+write(std::ostream& os, T data)
 {
     write(os, &data, 1);
 }
 
+/** Read a std::vector of POD-like variables from a stream */
 template <typename T>
 typename std::enable_if<std::is_trivially_copyable<T>::value>::type
 read(std::istream& is, std::vector<T>* data)
@@ -47,12 +60,25 @@ read(std::istream& is, std::vector<T>* data)
     read(is, &(*data)[0], size);
 }
 
+/** Write a std::vector of POD-like variables to a stream */
 template <typename T>
-void write(std::ostream& os, const std::vector<T>& data)
+typename std::enable_if<std::is_trivially_copyable<T>::value>::type
+write(std::ostream& os, const std::vector<T>& data)
 {
     write(os, data.size());
-    for (const auto& elem : data) {
-        write(os, elem);
+    write(os, &(data[0]), data.size());
+}
+
+/** Read a std::vector of variables from a stream */
+template <typename T>
+typename disable_if<std::is_trivially_copyable<T>::value>::type
+read(std::istream& is, std::vector<T>* data)
+{
+    size_t size;
+    read(is, &size);
+    data->resize(size);
+    for (auto& elem : (*data)) {
+        read(is, &elem);
     }
 }
 
