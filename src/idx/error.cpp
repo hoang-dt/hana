66d93eeb31b50c583fa1149bee35e2050034f866
hoangthaiduong <hoangthaiduong@gmail.com>
2016-07-17 21:29:54 -0700

    Rename idx to hana

--- src/idx/error.cpp
+++ src/idx/error.cpp
@@ -10,27 +10,23 @@ void set_error_msg(const char* msg) { copy(STR_REF(error_msg), StringRef(msg)); 
 
 const char* Error::get_error_msg() const
 {
-    const char* msg = Error::get_error_msg();
-    if (!msg) {
-        switch (code) {
-            case NoError: return "No error";
-            case FileNotFound: return "File not found";
-            case ParsingError: return "Parsing error";
-            case BlockNotFound: return "Block not found";
-            case BlockReadFailed: return "Block read failed";
-            case CompressionUnsupported: return "Compression unsupported";
-            case InvalidFormat: return "Invalid format";
-            case InvalidHzLevel: return "Invalid level";
-            case InvalidIdxFile: return "Invalid idx file";
-            case InvalidVolume: return "Invalid volume";
-            case InvalidCompression: return "Invalid compression";
-            case FieldNotFound: return "Field not found";
-            case TimeStepNotFound: return "Time step not found";
-            case VolumeTooBig: return "Volume too big";
-            default: return nullptr;
-        }
+    switch (code) {
+        case NoError: return "No error";
+        case FileNotFound: return "File not found";
+        case ParsingError: return "Parsing error";
+        case BlockNotFound: return "Block not found";
+        case BlockReadFailed: return "Block read failed";
+        case CompressionUnsupported: return "Compression unsupported";
+        case InvalidFormat: return "Invalid format";
+        case InvalidHzLevel: return "Invalid level";
+        case InvalidIdxFile: return "Invalid idx file";
+        case InvalidVolume: return "Invalid volume";
+        case InvalidCompression: return "Invalid compression";
+        case FieldNotFound: return "Field not found";
+        case TimeStepNotFound: return "Time step not found";
+        case VolumeTooBig: return "Volume too big";
+        default: return nullptr;
     }
-    return msg;
 }
 
 }
